655d4e68cbda7da76076667c049f81155b430ca8
David <xodnjs2549@naver.com>
2018-02-13 04:02:46 +0900

    edited. remove sysout

--- NumberBaseballGame/src/com/david/hwang/NumberBaseball.java
+++ NumberBaseballGame/src/com/david/hwang/NumberBaseball.java
@@ -2,6 +2,7 @@ package com.david.hwang;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class NumberBaseball {
 	public static void main(String[] args) {
@@ -14,54 +15,64 @@ public class NumberBaseball {
 		String randomNumber = "0";
 
 		// 컴퓨터 랜덤 수 생성
-//		for (int i = 0; i < 3; i++) {
-//			// overlap체크를 통과해야 다음 넘버 생성으로 진행
-//			{
-//				randomNumber = Integer.toString(nb.makeRandomNumber(1, 9)); //임의의 숫자 생성
-//				System.out.println("생성된 랜덤 숫자 : " + randomNumber);
-//			}while (nb.isOverlap(quizArr, randomNumber, i)); //생성된 임의의 숫자가 중복인지 체크
-//			System.out.println("중복되지 않는 랜덤 숫자 : " + randomNumber);
-//		}
-		//위의 방식으로 할 때, 간헐적으로 중복발생 무한 반복 현상 보임.
-		
+		// for (int i = 0; i < 3; i++) {
+		// // overlap체크를 통과해야 다음 넘버 생성으로 진행
+		// {
+		// randomNumber = Integer.toString(nb.makeRandomNumber(1, 9)); //임의의 숫자 생성
+		// System.out.println("생성된 랜덤 숫자 : " + randomNumber);
+		// }while (nb.isOverlap(quizArr, randomNumber, i)); //생성된 임의의 숫자가 중복인지 체크
+		// System.out.println("중복되지 않는 랜덤 숫자 : " + randomNumber);
+		// }
+		// 위의 방식으로 할 때, 간헐적으로 중복발생 무한 반복 현상 보임.
+
 		int i = 0;
-		while(i < 3) {
-			randomNumber = Integer.toString(nb.makeRandomNumber(1, 9)); //임의의 숫자 생성
-			System.out.println("생성된 랜덤 숫자 : " + randomNumber);
-			if(nb.isOverlap(quizArr, randomNumber, i)) {
+		while (i < 3) {
+			randomNumber = Integer.toString(nb.makeRandomNumber(1, 9)); // 임의의 숫자 생성
+			//System.out.println("생성된 랜덤 숫자 : " + randomNumber);
+			if (nb.isOverlap(quizArr, randomNumber, i)) {
 				continue;
-			}else {
+			} else {
 				i++;
 			}
 		}
-		System.out.println("컴퓨터 랜덤 수 : " + Arrays.toString(quizArr));
+		//System.out.println("컴퓨터 랜덤 수 : " + Arrays.toString(quizArr));
 
-		// 사용자 입력 처리
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("숫자를 입력해주세요 ex)123 : ");
-		String userInputNum = scanner.next();
-		System.out.println(userInputNum);
+		while (scoreArr[0] < 3) {
+			nb.init(answerArr, scoreArr);
+			// 사용자 입력 처리
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("숫자를 입력해주세요 ex)123 : ");
+			String userInputNum = scanner.next();
+			//System.out.println(userInputNum);
 
-		// 사용자가 입력한 수(문자열)를 배열에 하나씩 쪼개어 넣는다.
-		answerArr = userInputNum.split("(?!^)");
-		System.out.println("사용자가 입력한 수 : " + Arrays.toString(answerArr));
-		// System.out.println(nb.makeRandomNumber(1,9));
+			// 사용자가 입력한 수(문자열)를 배열에 하나씩 쪼개어 넣는다.
+			answerArr = userInputNum.split("(?!^)");
+			//System.out.println("사용자가 입력한 수 : " + Arrays.toString(answerArr));
+			// System.out.println(nb.makeRandomNumber(1,9));
 
-		// 컴퓨터와 사용자의 수 비교
-		// if (quizArr[0].equals(answerArr[0])) {
-		// strike++;
-		// System.out.println("no1");
-		// } else if (quizArr[0].equals(answerArr[1])) {
-		// ball++;
-		// System.out.println("no2");
-		// } else if (quizArr[0].equals(answerArr[2])) {
-		// ball++;
-		// System.out.println("no3");
-		// }
+			// 컴퓨터와 사용자의 수 비교
+			// if (quizArr[0].equals(answerArr[0])) {
+			// strike++;
+			// System.out.println("no1");
+			// } else if (quizArr[0].equals(answerArr[1])) {
+			// ball++;
+			// System.out.println("no2");
+			// } else if (quizArr[0].equals(answerArr[2])) {
+			// ball++;
+			// System.out.println("no3");
+			// }
 
-		nb.compare(quizArr, answerArr, scoreArr);
-		System.out.println(Arrays.toString(scoreArr));
-		nb.result(scoreArr);
+			nb.compare(quizArr, answerArr, scoreArr);
+			//System.out.println(Arrays.toString(scoreArr));
+			nb.result(scoreArr);
+		}
+	}
+	
+	public void init(String[] answerArr, int[] scoreArr ) {
+		Arrays.fill(answerArr, "0");
+		Arrays.fill(scoreArr, 0);
+		//System.out.println(Arrays.toString(answerArr));
+		//System.out.println(Arrays.toString(scoreArr));
 	}
 
 	// 컴퓨터의 랜덤 수와 사용자가 입력한 수를 비교
@@ -95,18 +106,18 @@ public class NumberBaseball {
 
 	public void result(int[] scoreArr) {
 		if (scoreArr[0] != 0) {
-			if(scoreArr[0] == 3) {
-				System.out.println();
-			}else {
-				System.out.println(scoreArr[0] + "스트라이크");
+			if (scoreArr[0] == 3) {
+				System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			} else {
+				System.out.print(scoreArr[0] + "스트라이크 ");
 			}
 		}
-		
+
 		if (scoreArr[1] != 0) {
 			System.out.println(scoreArr[1] + "볼");
 		}
-		
-		if((scoreArr[0] == 0) && (scoreArr[1] == 0)){
+
+		if ((scoreArr[0] == 0) && (scoreArr[1] == 0)) {
 			System.out.println("낫싱");
 		}
 	}
@@ -114,31 +125,32 @@ public class NumberBaseball {
 	/*
 	 * num1은 랜덤 수의 하한값, num2는 랜덤 수의 상한값. makeRandomNumber(1, 9)이면, 1~9까지의 랜덤한 수를 반환
 	 */
-	public int makeRandomNumber(int num1, int num2) {
+	public int makeRandomNumber(int min, int max) {
 		// int randomNumber = 0;
 		// {
 		// randomNumber = (int)(Math.random() * 10); //먼저 10을 곱하고 int로 변환. int로 먼저 변환하면
 		// 0으로 출력.
 		// System.out.println(randomNumber);
 		// }while(!(randomNumber == 0 || randomNumber == 10));
-		int randomNumber = (int) (Math.random() * (num2 - num1 + 1)) + num1;
+		// int randomNumber = (int) (Math.random() * (num2 - num1 + 1)) + num1;
+		int randomNumber = ThreadLocalRandom.current().nextInt(min, max + 1);
 		return randomNumber;
 	}
 
 	public boolean isOverlap(String[] quizArr, String randomNumber, int index) {
-		System.out.println("중복발생0");
+		//System.out.println("중복발생0");
 		for (int i = 0; i < 3; i++) {
-			System.out.println("중복발생1");
+			//System.out.println("중복발생1");
 			if (!(quizArr[i].equals("0")) && (quizArr[i].equals(randomNumber))) {
-				System.out.println("중복발생2");
+				//System.out.println("중복발생2");
 				return true;
 			}
 		}
-		insert(quizArr, randomNumber, index); //중복되지 않는 숫자, quiz배열에 삽입
-		System.out.println("not중복발생");
+		insert(quizArr, randomNumber, index); // 중복되지 않는 숫자, quiz배열에 삽입
+		//System.out.println("not중복발생");
 		return false;
 	}
-	
+
 	public void insert(String[] quizArr, String randomNumber, int index) {
 		quizArr[index] = randomNumber;
 	}
