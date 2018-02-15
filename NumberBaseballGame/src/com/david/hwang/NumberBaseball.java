7052f6a95cf0fe753514f8b479a46c685ca0654b
DESKTOP-I80NLN2\David <xodnjs2549@naver.com>
2018-02-16 05:00:47 +0900

    edited. string arr change to int arr

--- NumberBaseballGame/src/com/david/hwang/NumberBaseball.java
+++ NumberBaseballGame/src/com/david/hwang/NumberBaseball.java
@@ -5,49 +5,54 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class NumberBaseball {
+	/* 선언 및 초기화 */
+	int[] quizArr = { 0, 0, 0 }; //컴퓨터가 생성할 임의의 수 3개가 들어갈 배열
+	int[] answerArr = { 0, 0, 0 }; //사용자가 입력한 3자리 수가 들어갈 배열
+	int[] scoreArr = { 0, 0 }; //[0]:strike, [1]:ball
+	int randomNumber = 0;
+
 	public static void main(String[] args) {
-		NumberBaseball nb = new NumberBaseball();
-
-		/* 선언 및 초기화 */
-		String[] quizArr = new String[] { "0", "0", "0" }; // 컴퓨터가 생성할 임의의 수 3개가 들어갈 배열
-		String[] answerArr = new String[] { "0", "0", "0" }; // 사용자가 입력한 3자리 수가 들어갈 배열
-		int[] scoreArr = new int[] { 0, 0 }; // [0]:strike, [1]:ball
-		String randomNumber = "0";
-
-		// 컴퓨터 랜덤 수 생성
-		int i = 0;
-		while (i < 3) {
-			randomNumber = Integer.toString(nb.makeRandomNumber(1, 9)); // 임의의 숫자 생성
-			if (nb.isOverlap(quizArr, randomNumber, i)) {
-				continue;
-			} else {
-				i++;
-			}
-		}
-
-		while (scoreArr[0] < 3) {
-			nb.init(answerArr, scoreArr);
-			// 사용자 입력 처리
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("숫자를 입력해주세요 ex)123 : ");
-			String userInputNum = scanner.next();
-
-			// 사용자가 입력한 수(문자열)를 배열에 하나씩 쪼개어 넣는다.
-			answerArr = userInputNum.split("(?!^)");
-			// 컴퓨터와 사용자의 수 비교
-			nb.compare(quizArr, answerArr, scoreArr);
-			nb.result(scoreArr);
+		NumberBaseball nb = new NumberBaseball();  //NumberBaseball 객체 생성
+		
+		nb.insertRandomNumber(nb.quizArr, nb.randomNumber);  //컴퓨터 랜덤 수 생성
+		
+		while (nb.scoreArr[0] < 3) {
+			nb.init(nb.answerArr, nb.scoreArr);  //이전 사용자
+			//nb.splitNumber(nb.inputNumber());  //입력받은 숫자를 각각 한 자리 숫자로 나누기
+			//첫 번째 매개변수가 별로(?)다. 각각의 리턴값을 변수로 만들어서 넣자니 줄이 늘어난다.
+			nb.insertNumber(nb.splitNumber(nb.inputNumber()), nb.answerArr);
+			nb.compare(nb.quizArr, nb.answerArr, nb.scoreArr);
+			nb.result(nb.scoreArr);
 		}
 	}
 
-	// 사용자 입력 값이 담긴 배열과 점수 배열을 초기화
-	public void init(String[] answerArr, int[] scoreArr) {
-		Arrays.fill(answerArr, "0");
+	public int inputNumber() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자를 입력해주세요 ex)123 : ");
+		int userInputNumber = scanner.nextInt();
+		return userInputNumber;
+	}
+
+	public int[] splitNumber(int userInputNumber) {
+		int[] userInputNumberArr = { 0, 0, 0 };
+		userInputNumberArr[0] = userInputNumber / 100; // 백의자리
+		userInputNumberArr[1] = ( userInputNumber / 10 ) % 10 ; //십의자리
+		userInputNumberArr[2] = userInputNumber % 10; //일의자리
+		return userInputNumberArr;
+	}
+	
+	public void insertNumber(int[] splittedNumberArr, int[] answerArr) {
+		System.arraycopy(splittedNumberArr, 0, answerArr, 0, 3);
+	}
+
+	// 이전에 입력된 사용자 입력 수 및 점수 초기화
+	public void init(int[] answerArr, int[] scoreArr) {
+		Arrays.fill(answerArr, 0);
 		Arrays.fill(scoreArr, 0);
 	}
 
 	// 컴퓨터의 랜덤 수와 사용자가 입력한 수를 비교
-	public void compare(String[] quizArr, String[] answerArr, int[] scoreArr) {
+	public void compare(int[] quizArr, int[] answerArr, int[] scoreArr) {
 		for (int i = 0; i < 3; i++) {
 			if (isStrike(quizArr, answerArr, scoreArr, i)) {
 				continue;
@@ -57,8 +62,8 @@ public class NumberBaseball {
 		}
 	}
 
-	public boolean isStrike(String[] quizArr, String[] answerArr, int[] scoreArr, int index) {
-		if (quizArr[index].equals(answerArr[index])) {
+	public boolean isStrike(int[] quizArr, int[] answerArr, int[] scoreArr, int index) {
+		if (quizArr[index] == answerArr[index]) {
 			scoreArr[0]++; // strike++;
 			return true;
 		} else {
@@ -66,9 +71,9 @@ public class NumberBaseball {
 		}
 	}
 
-	public void isBall(String[] quizArr, String[] answerArr, int[] scoreArr, int index) {
+	public void isBall(int[] quizArr, int[] answerArr, int[] scoreArr, int index) {
 		for (int i = 0; i < 3; i++) {
-			if (!(i == index) && quizArr[index].equals(answerArr[i])) {
+			if (!(i == index) && quizArr[index] == answerArr[i]) {
 				scoreArr[1]++; // ball++;
 			}
 		}
@@ -84,12 +89,14 @@ public class NumberBaseball {
 		}
 
 		if (scoreArr[1] != 0) {
-			System.out.println(scoreArr[1] + "볼");
+			System.out.print(scoreArr[1] + "볼");
 		}
 
 		if ((scoreArr[0] == 0) && (scoreArr[1] == 0)) {
-			System.out.println("낫싱");
+			System.out.print("낫싱");
 		}
+		
+		System.out.println("");
 	}
 
 	/*
@@ -100,9 +107,21 @@ public class NumberBaseball {
 		return randomNumber;
 	}
 
-	public boolean isOverlap(String[] quizArr, String randomNumber, int index) {
+	public void insertRandomNumber(int[] quizArr, int randomNumber) {
+		int i = 0;
+		while (i < 3) {
+			randomNumber = makeRandomNumber(1, 9); // 임의의 숫자 생성
+			if (isOverlap(quizArr, randomNumber, i)) {
+				continue;
+			} else {
+				i++;
+			}
+		}
+	}
+
+	public boolean isOverlap(int[] quizArr, int randomNumber, int index) {
 		for (int i = 0; i < 3; i++) {
-			if (!(quizArr[i].equals("0")) && (quizArr[i].equals(randomNumber))) {
+			if (!(quizArr[i] == 0) && (quizArr[i] == randomNumber)) {
 				return true;
 			}
 		}
@@ -110,7 +129,7 @@ public class NumberBaseball {
 		return false;
 	}
 
-	public void insert(String[] quizArr, String randomNumber, int index) {
+	public void insert(int[] quizArr, int randomNumber, int index) {
 		quizArr[index] = randomNumber;
 	}
 }
